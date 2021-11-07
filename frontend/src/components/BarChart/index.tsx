f101040e163d8bf3cb0169a5e0ca5ae301feffb9
levircesar <levirlemos@gmail.com>
2021-11-07 18:24:33 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,14 +1,62 @@
-import Chart from 'react-apexcharts';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Chart from "react-apexcharts";
+import { SaleSuccess } from "types/sale";
+import { round } from "utils/format";
+import { BASE_URL } from "utils/requests";
 
-const BarChat  = () => {
+type SeriesData = {
+  name: string;
+  data: number[];
+};
+
+type ChartData = {
+  labels: {
+    categories: string[];
+  };
+  series: SeriesData[];
+};
+
+const BarChat = () => {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: [],
+    },
+    series: [
+      {
+        name: "",
+        data: [],
+      },
+    ],
+  });
+
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then((response) => {
+      const data = response.data as SaleSuccess[];
+      const myLabels = data.map((x) => x.sellerName);
+      const mySeries = data.map((x) => round((100.0 * x.deals) / x.visited, 1));
+      setChartData({
+        labels: {
+          categories: myLabels,
+        },
+        series: [
+          {
+            name: "% Success",
+            data: mySeries,
+          },
+        ],
+      });
+    });
+  }, []);
   const options = {
     plotOptions: {
       bar: {
-          horizontal: true,
-      }
+        horizontal: true,
+      },
     },
   };
 
+  /*
 const mockData = {
     labels: {
       categories: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
@@ -20,15 +68,16 @@ const mockData = {
       }
     ]
   };
+  */
 
   return (
-    <Chart 
-      options={{ ...options, xaxis: mockData.labels }}
-      series={mockData.series}
+    <Chart
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
       type="bar"
       height="240"
     />
   );
-}
+};
 
 export default BarChat;
